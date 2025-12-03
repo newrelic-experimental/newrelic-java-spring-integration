623bbd0cc9bb15b1fa1319c06c89b16be9014f33
Doug Hilpipre <dhilpipre@newrelic.com>
2025-12-03 13:31:36 -0600

    changed to lowercase

---
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/TcpInboundGateway.java
@@ -0,0 +1,21 @@
+package org.springframework.integration.ip.tcp;
+
+import org.springframework.integration.Message;
+import org.springframework.integration.gateway.MessagingGatewaySupport;
+
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TransactionNamePriority;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@Weave
+public abstract class TcpInboundGateway extends MessagingGatewaySupport {
+
+	@Trace(dispatcher=true)
+	public boolean onMessage(Message<?> message) {
+		NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.FRAMEWORK_HIGH, true, "TcpInboundGateway", new String[] {"TcpInboundGateway",getComponentName()});
+		NewRelic.getAgent().getTracedMethod().setMetricName(new String[] {"Custom","TcpInboundGateway",getComponentName(),"onMessage"});
+		return Weaver.callOriginal();
+	}
+}

0e95f21aa3a0cb717d1f00b67ffc6dbc8bef4417
Doug Hilpipre <dhilpipre@newrelic.com>
2025-12-12 15:33:50 -0600

    remove unneeded module since it causes problems

--- spring-jms/src/main/java/org/springframework/jms/core/JmsMessagingTemplate_Instrumentation.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.jms.core;
-
-import com.newrelic.api.agent.weaver.Weave;
-
-@Weave(originalName = "org.springframework.jms.core.JmsMessagingTemplate")
-public class JmsMessagingTemplate_Instrumentation {
-
-
-}
